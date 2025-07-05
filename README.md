ddbfa2b5833a871d47d707502ce3c1138fa9d276
Kevin27954 <kevinl33078@gmail.com>
2025-07-04 21:44:04 -0400

    Init Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Notes
+===
+
+This will be the place where I write down what I learn and hated or whatever about SpringBoot so that I can put it on my resume after wards and get a JOB UWU HELP ME PLEASE!
+
+So when you start a SpringBoot project as a web project, you need to have (all same versions of courses or the latest):
+- spring-starter-web
+- spring-web
+
+Spring-Web 
+===
+This one gives you the annotations to help build the web.
+
+Spring-Starter-Web
+===
+This one gives you the functions that the annotations will use to start the web, I'm guessing.
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+    eclipse
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.formkio"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework:spring-web:6.2.8")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.5.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
